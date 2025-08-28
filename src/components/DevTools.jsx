fa7dac88469b147cf37b2b417a656fc2bfb3120c
shift-primal <kasper@haugestol.com>
2025-08-28 09:19:53 +0200

    Fiksa devtools åpne/lukke

--- src/components/DevTools.jsx
+++ src/components/DevTools.jsx
@@ -1,10 +1,13 @@
-import { use } from "react";
+import { use, useState } from "react";
 import { PokemonContext } from "../contexts/PokemonContext";
 import { getNextPokemonId, getPrevPokemonId } from "../utils/controls";
+import { ChevronFirst, ChevronLast } from "lucide-react";
 
 export default function DevTools() {
 	const { currentPokemon, showContent, setPokemonToFetch, onLoadData, setMenuOpen, setShowContent } =
 		use(PokemonContext);
+
+	const [toolsOpen, setToolsOpen] = useState(false);
 
 	const handleForm = (formData) => setPokemonToFetch(formData.get("newPokemon"));
 	const handleNext = () => setPokemonToFetch(getNextPokemonId(currentPokemon));
@@ -13,62 +16,74 @@ export default function DevTools() {
 	const logOnLoadData = () => console.log("OnLoad Data:", onLoadData);
 	const toggleMenu = () => setMenuOpen((prev) => !prev);
 	const toggleShowContent = () => setShowContent((prev) => !prev);
+	const toggleTools = () => setToolsOpen((prev) => !prev);
 
 	return (
 		<div
 			id="controls-container"
-			className="absolute top-1/2 -translate-y-1/2 right-10 gap-x-2 h-fit w-fit max-w-xl p-16 bg-background rounded-xl flex flex-wrap items-center justify-center gap-y-2 z-100"
+			className="absolute bottom-10 left-10 h-fit w-fit max-w-xl p-4 bg-background rounded-xl flex items-center z-100"
 		>
-			<form action={handleForm}>
-				<input
-					type="text"
-					name="newPokemon"
+			<button
+				onClick={toggleTools}
+				className="left-0 w-fit h-full focus:outline-none p-2 cursor-pointer"
+			>
+				{!toolsOpen ? <ChevronLast /> : <ChevronFirst />}
+			</button>
+			<div
+				id="controls"
+				className={`flex flex-wrap items-center justify-center gap-2 ${!toolsOpen ? "hidden" : ""}`}
+			>
+				<form action={handleForm}>
+					<input
+						type="text"
+						name="newPokemon"
+						disabled={!showContent}
+						placeholder="Search pokemon..."
+						className="min-w-50 h-full border-2 rounded-md focus:outline-none p-2 disabled:cursor-not-allowed disabled:opacity-50 bg-background hover:bg-gray-100 focus:bg-gray-200"
+					/>
+				</form>
+				<button
+					className="border-2 px-3 py-1 cursor-pointer disabled:cursor-not-allowed disabled:opacity-50 rounded-md bg-gray-50 hover:bg-gray-100"
+					onClick={handlePrev}
 					disabled={!showContent}
-					placeholder="Search pokemon..."
-					className="min-w-50 h-full border-2 rounded-md focus:outline-none p-2 disabled:cursor-not-allowed disabled:opacity-50 bg-background hover:bg-gray-100 focus:bg-gray-200"
-				/>
-			</form>
-			<button
-				className="border-2 px-3 py-1 cursor-pointer disabled:cursor-not-allowed disabled:opacity-50 rounded-md bg-gray-50 hover:bg-gray-100"
-				onClick={handlePrev}
-				disabled={!showContent}
-			>
-				Previous
-			</button>
-			<button
-				className="border-2 px-3 py-1 cursor-pointer disabled:cursor-not-allowed disabled:opacity-50 rounded-md bg-gray-50 hover:bg-gray-100"
-				onClick={handleNext}
-				disabled={!showContent}
-			>
-				Next
-			</button>
-			<button
-				className="border-2 px-3 py-1 cursor-pointer disabled:cursor-not-allowed disabled:opacity-50 rounded-md bg-gray-50 hover:bg-gray-100"
-				onClick={logCurrentId}
-				disabled={!showContent}
-			>
-				Log ID
-			</button>
-			<button
-				className="border-2 px-3 py-1 cursor-pointer disabled:cursor-not-allowed disabled:opacity-50 rounded-md bg-gray-50 hover:bg-gray-100"
-				onClick={logOnLoadData}
-				disabled={!showContent}
-			>
-				Log onLoadData
-			</button>
-			<button
-				className="border-2 px-3 py-1 cursor-pointer disabled:cursor-not-allowed disabled:opacity-50 rounded-md bg-gray-50 hover:bg-gray-100"
-				onClick={toggleMenu}
-				disabled={!showContent}
-			>
-				Toggle Menu
-			</button>
-			<button
-				className="border-2 px-3 py-1 cursor-pointer disabled:cursor-not-allowed disabled:opacity-50 rounded-md bg-gray-50 hover:bg-gray-100"
-				onClick={toggleShowContent}
-			>
-				Toggle Showcontent
-			</button>
+				>
+					Previous
+				</button>
+				<button
+					className="border-2 px-3 py-1 cursor-pointer disabled:cursor-not-allowed disabled:opacity-50 rounded-md bg-gray-50 hover:bg-gray-100"
+					onClick={handleNext}
+					disabled={!showContent}
+				>
+					Next
+				</button>
+				<button
+					className="border-2 px-3 py-1 cursor-pointer disabled:cursor-not-allowed disabled:opacity-50 rounded-md bg-gray-50 hover:bg-gray-100"
+					onClick={logCurrentId}
+					disabled={!showContent}
+				>
+					Log ID
+				</button>
+				<button
+					className="border-2 px-3 py-1 cursor-pointer disabled:cursor-not-allowed disabled:opacity-50 rounded-md bg-gray-50 hover:bg-gray-100"
+					onClick={logOnLoadData}
+					disabled={!showContent}
+				>
+					Log onLoadData
+				</button>
+				<button
+					className="border-2 px-3 py-1 cursor-pointer disabled:cursor-not-allowed disabled:opacity-50 rounded-md bg-gray-50 hover:bg-gray-100"
+					onClick={toggleMenu}
+					disabled={!showContent}
+				>
+					Toggle Menu
+				</button>
+				<button
+					className="border-2 px-3 py-1 cursor-pointer disabled:cursor-not-allowed disabled:opacity-50 rounded-md bg-gray-50 hover:bg-gray-100"
+					onClick={toggleShowContent}
+				>
+					Toggle Showcontent
+				</button>
+			</div>
 		</div>
 	);
 }
